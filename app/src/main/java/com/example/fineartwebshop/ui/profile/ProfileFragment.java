6fb0c8c628f771dfe20e0d8b0ad40c53ab79165b
Adorján Pál <adorjan3270@gmail.com>
2025-05-08 13:49:49 +0200

    profile page

--- app/src/main/java/com/example/fineartwebshop/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/fineartwebshop/ui/profile/ProfileFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.example.fineartwebshop.R;
 import com.example.fineartwebshop.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
@@ -23,6 +26,17 @@ public class ProfileFragment extends Fragment {
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        TextView usernameTextView = binding.usernameTextView;
+        TextView nameTextView = binding.nameTextView;
+
+        Glide.with(binding.getRoot().getContext())
+                .load(R.drawable.account_circle_24px)
+                .into(binding.profilePicture);
+
+        // Set dummy data
+        usernameTextView.setText("DummyUsername");
+        nameTextView.setText("Dummy Name");
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
